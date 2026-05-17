27a51a4497bea6bd4fa969f10ad6bb6504becd50
Seb <contact@cloveria.fr>
2026-05-17 18:23:10 +0200

    hardcode API URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = (import.meta.env.VITE_API_URL || '') + '/api';
+const API_URL = 'https://cloveria-fichetech.onrender.com/api';
 
 function getToken() {
   return localStorage.getItem('token');
